8fc345322e02ffbb8e54332e71c050b38ea697d5
Blake Garner <blake@netjibbing.com>
2026-04-25 14:03:15 -0700

    Pin swift-argument-parser to 1.5.x
    1.7.1 uses SendableMetatype behind #if compiler(>=6.2) but that type is
    absent in some Swift 6.2 prerelease toolchains, breaking the macOS 15 CI build.

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ let package = Package(
     name: "SwiftKHD",
     platforms: [.macOS(.v13)],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser", from: "1.3.0"),
+        .package(url: "https://github.com/apple/swift-argument-parser", .upToNextMinor(from: "1.5.0")),
     ],
     targets: [
         .target(
